a0f49bf413427eb94f646f1ebcdb841baa18825e
secos <secos>
2004-04-30 14:25:35 +0000

    Kommentar verändert

--- Uebungen/Uebung02/src/alpiv/turmuhr/StartClock.java
+++ Uebungen/Uebung02/src/alpiv/turmuhr/StartClock.java
@@ -7,8 +7,8 @@ public class StartClock
 		// Erzeugt eine neue Turmuhr:
 		Turmuhr turmuhr = new TurmuhrFactory().createTurmuhr();
 
-		// Erzeugt einen neuen Uhrentreiber, der die Turmuhr steuert:
-		// Pro Sekunde Realzeit werden 50 Sekunden simuliert
+		// Erzeugt einen neuen Uhrentreiber, der die Turmuhr steuert
+		// (in einer Sekunde Realzeit werden 50 Sekunden simuliert)
 		ClockDriver clockDriver = new ClockDriver(turmuhr, 50);
 
 		// Startet den Treiber-Thread:
