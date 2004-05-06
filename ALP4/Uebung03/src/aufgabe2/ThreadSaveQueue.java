89028004f3bc43946019d23eebfa1b49338ea59b
secos <secos>
2004-05-06 14:09:36 +0000

    no message

---
+++ ALP4/Uebung03/src/aufgabe2/ThreadSaveQueue.java
@@ -0,0 +1,68 @@
+package aufgabe2;
+
+class ThreadSaveQueue
+{
+	private class QueueElement
+	{
+		volatile Object value;
+		volatile QueueElement next = null;
+
+		QueueElement(Object val)
+		{
+			this.value = val;
+		}
+	}
+
+	// Verweis auf das letzte Element der Liste
+	// ist dieses null, dann ist die Schlange leer.
+	volatile private QueueElement last = null;
+
+	public void add(Object obj)
+	{   // Erzeugen des einzufügenden Knotens kann parralel ausgeführt werden!
+		QueueElement nw = new QueueElement(obj);
+
+		// Eintragen muß unteilbar sein:
+		synchronized (this)
+		{
+			if (last == null)
+			{
+				nw.next = nw;
+			}
+
+			else
+			{
+				nw.next = last.next;
+				last.next = nw;
+			}
+
+			last = nw;
+		}
+	}
+
+	public Object remove()
+	{
+		// Nur lesender Zugriff, kann auch bei gleichzeitigem add
+		// noch korrekt parellel ausgeführt werden.
+		if (last == null)
+		{
+			return null;
+		}
+
+		synchronized (this)
+		{
+			if (last.next == last)
+			{
+				Object save = last.value;
+				last = null;
+				return save;
+			}
+
+			else
+			{
+				Object val = last.next.value;
+				last.next = last.next.next;
+				return val;
+			}
+		}
+	}
+}
