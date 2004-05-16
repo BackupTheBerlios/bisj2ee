bca8f7a97ee678280c37916c11593e1a2ca179ed
secos <secos>
2004-05-16 11:55:33 +0000

    no message

---
+++ ALP4/Uebung04/src/aufgabe02/Semaphore.java
@@ -0,0 +1,34 @@
+package aufgabe02;
+
+public class Semaphore
+{
+	//  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+	//  |                 Instanzvariablen                  |   \\
+	//  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
+
+	private int threadCapacity = 1;
+
+	//  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+	//  |                     Methoden                      |   \\
+	//  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
+
+	public synchronized void OCCUPY()
+	{
+		if (--threadCapacity < 0)
+		{
+			try
+			{
+				wait();
+			}
+			catch (InterruptedException e)
+			{
+			}
+		}
+	}
+
+	public synchronized void RELEASE()
+	{
+		threadCapacity++;
+		notify();
+	}
+}
