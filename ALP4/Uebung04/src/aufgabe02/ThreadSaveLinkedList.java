ce0334cece118c098b3a1b54ed85469571a43d05
secos <secos>
2004-05-16 11:55:33 +0000

    no message

--- ALP4/Uebung04/src/aufgabe02/ThreadSaveLinkedList.java
+++ ALP4/Uebung04/src/aufgabe02/ThreadSaveLinkedList.java
@@ -5,63 +5,87 @@ import java.util.NoSuchElementException;
 public class ThreadSaveLinkedList
 implements Set
 {
-	private final ListElement first = new ListElement(null);
-	private volatile ListElement last = first;
+	//  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+	//  |                 Instanzvariablen                  |   \\
+	//  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
 
-	public void add(Object obj)
+	private final ListElement firstNode = new ListElement(null);
+	private volatile ListElement lastNode = firstNode;
+
+	//  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+	//  |                     Modifiers                     |   \\
+	//  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
+
+	public void add(Object value)
 	{
-		ListElement neu = new ListElement(obj);
+		if (value == null) return;
 
-		synchronized (last)
+		ListElement newNode = new ListElement(value);
+
+		synchronized (this)
 		{
-			last = last.next = neu;
+			lastNode = lastNode.next = newNode;
 		}
 	}
 
-	public void remove(Object obj)
+	public void remove(Object value)
 	{
-		ListElement previous;
-		ListElement index;
+		if (value == null) return;
 
-		synchronized (first)
-		{
-			previous = first;
-			index = first.next;
-		}
+		firstNode.OCCUPY();
 
-		while (index != null)
+		ListElement index = firstNode;
+		ListElement nextNode = index.next;
+
+		while (nextNode != null)
 		{
-			synchronized (previous)
+			if (value.equals(nextNode.value))
 			{
-				synchronized (index)
+				synchronized (this)
 				{
-					if (index.value.equals(obj))
+					if (nextNode == lastNode)
 					{
-						previous.next = index.next;
+						index.next = null;
+						lastNode = index;
 
-						if (index == last)
-							last = previous;
-
+						index.RELEASE();
 						return;
 					}
-					else
-					{
-						previous = index;
-						index = index.next;
-
-					}
 				}
+
+				index.next = nextNode.next;
+
+				index.RELEASE();
+				return;
 			}
+
+			nextNode = nextNode.next;
+
+			if (nextNode != null) nextNode.OCCUPY();
+			index.RELEASE();
+
+			index = index.next;
 		}
+
+		// Element was not found:
+		index.RELEASE();
 	}
+
+	//  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+	//  |                     Producers                     |   \\
+	//  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
 
 	public Iterator iterator()
 	{
 		return new MyIterator();
-
 	}
 
+	//  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+	//  |                   Inner Classes                   |   \\
+	//  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
+
 	private class ListElement
+	extends Semaphore
 	{
 		private volatile ListElement next;
 		private volatile Object value;
@@ -75,16 +99,24 @@ implements Set
 	private class MyIterator
 	implements Iterator
 	{
-		private ListElement index = first.next;
+		//  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+		//  |                 Instanzvariablen                  |   \\
+		//  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
+
+		private ListElement index = firstNode.next;
+
+		//  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+		//  |                     Scanners                      |   \\
+		//  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
 
 		public Object next()
 		throws NoSuchElementException
 		{
-			if (index == null) throw new NoSuchElementException();
+			if (!hasNext()) throw new NoSuchElementException();
 
 			Object value = index.value;
-			index = index.next;
 
+			index = index.next;
 			return value;
 		}
 
@@ -92,7 +124,163 @@ implements Set
 		{
 			return index != null;
 		}
+	}
+
+	//  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+	//  |                       Test                        |   \\
+	//  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
+
+	public static void main(String[] args)
+	{
+		final ThreadSaveLinkedList list = new ThreadSaveLinkedList();
+
+		final Thread[] threads = new Thread[100];
+
+		for (int i = 0; i < threads.length; i++)
+		{
+			final int k = i;
+			threads[i] = new Thread(new Runnable()
+			{
+				public void run()
+				{
+
+					list.add(new Integer(k));
+
+				}
+
+			});
+
+		}
+
+		for (int i = 0; i < threads.length; i++)
+		{
+			threads[i].start();
+
+		}
+
+		for (int i = 0; i < threads.length; i++)
+		{
+			try
+			{
+				threads[i].join();
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+
+		}
+
+		Iterator it = list.iterator();
+
+		int count = 0;
+		while (it.hasNext())
+		{
+			count++;
+			System.out.print("" + it.next() + ", ");
+		}
+
+		System.out.println("\nLänge: " + count);
+
+		for (int i = threads.length - 1; i >= 0; i--)
+		{
+			final int k = i;
+			threads[i] = new Thread(new Runnable()
+			{
+				public void run()
+				{
+					list.remove(new Integer(k));
+
+				}
+
+			});
+
+		}
+
+		for (int i = threads.length / 2; i >= 0; i--)
+		{
+			threads[i].start();
+		}
+
+		for (int i = 0; i <= threads.length / 2; i++)
+		{
+			try
+			{
+				threads[i].join();
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+
+		}
+
+		it = list.iterator();
+
+		count = 0;
+		while (it.hasNext())
+		{
+			count++;
+			System.out.print("" + it.next() + ", ");
+		}
+
+		System.out.println("\nLänge: " + count);
+
+
+		threads[0] = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				list.remove(new Integer(154));
+			}
+
+		});
+
+		threads[1] = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				list.remove(new Integer(99));
+			}
+
+		});
+
+		threads[2] = new Thread(new Runnable()
+		{
+			public void run()
+			{
+				list.remove(new Integer(98));
+			}
+
+		});
+
+		threads[0].start();
+		threads[1].start();
+		threads[2].start();
+
+		try
+		{
+			threads[0].join();
+			threads[1].join();
+			threads[2].join();
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+
+		it = list.iterator();
+
+		count = 0;
+		while (it.hasNext())
+		{
+			count++;
+			System.out.print("" + it.next() + ", ");
+		}
+
+		System.out.println("\nLänge: " + count);
 
 	}
+
 
 }
---
+++ ALP4/Uebung04/src/aufgabe03/ThreadSaveSet.java
@@ -0,0 +1,78 @@
+package aufgabe03;
+
+public class ThreadSaveSet
+implements Set
+{
+	private final Object[] values;
+	private final Integer[] locks;
+
+	public ThreadSaveSet(int size)
+	{
+		values = new Object[size];
+
+		locks = new Integer[size];
+
+		for (int i = 0; i < locks.length; i++)
+		{
+			locks[i] = new Integer(i);
+		}
+	}
+
+	public void add(Object obj)
+	throws Overflow
+	{
+		int index = obj.hashCode() % values.length;
+
+		add(obj, index, 0);
+
+	}
+
+	private void add(Object obj, int index, int iteration)
+	throws Overflow
+	{
+		if (iteration == values.length) throw new Overflow();
+
+		synchronized (locks[index])
+		{
+			if (values[index] == null)
+				values[index] = obj;
+			else
+				add(obj, (index + 1) % values.length, iteration + 1);
+		}
+	}
+
+	public boolean contains(Object obj)
+	{
+		return contains(obj, obj.hashCode() % values.length, 0);
+	}
+
+	private boolean contains(Object obj, int index, int iteration)
+	{
+		if (iteration == values.length) return false;
+		if (values[index] == null) return false;
+
+		return values[index].equals(obj) || contains(obj, (index + 1) % values.length, iteration + 1);
+	}
+
+	public static void main(String[] args)
+	{
+		ThreadSaveSet testSet = new ThreadSaveSet(10);
+
+		for (int i = 1; i <= 11; i++)
+		{
+			try
+			{
+				testSet.add(new Integer(i));
+			}
+			catch (Overflow overflow)
+			{
+				System.out.println("Overflow beim Einfügen von " + i);
+			}
+
+			System.out.println("Contains " + i + " ? " + testSet.contains(new Integer(i)));
+			System.out.println("Contains " + (i * i) + " ? " + testSet.contains(new Integer(i * i)));
+		}
+
+
+	}
+}
