050273fc5811c45dd78721153902625a7ecc2dd2
secos <secos>
2004-06-21 21:25:00 +0000

    no message

--- ALP4/Uebung09/src/BooleanSemaphore.java
+++ ALP4/Uebung09/src/BooleanSemaphore.java
@@ -1,27 +1,42 @@
 
+/**
+ * Implementation einer boolschen Semaphore.
+ */
 public class BooleanSemaphore
 {
-	private boolean resource = true;
 
+	//  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+	//  |                 Instanzvariablen                  |   \\
+	//  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
+
+	private boolean available = true;
+
+	//  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+	//  |                     Methoden                      |   \\
+	//  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
+
+	/**
+	 * Falls eine Freigabe zu Zeit nicht möglich ist (!available), wird der Thread zunächst in den Wartezustand
+	 * versetzt bis die Freigabe möglich ist.
+	 * <p/>
+	 * Sobald die Freigabe möglich ist, wird available auf false gesetzt und der Monitor verlassen.
+	 */
 	public synchronized void P()
+	throws InterruptedException
 	{
-		if (!resource)
-			try
-			{
-				wait();
-			}
-			catch (InterruptedException e)
-			{
-				e.printStackTrace();
-			}
+		if (!available)
+			wait();
 
-		assert resource;
-		resource = false;
+		available = false;
 	}
 
+	/**
+	 * Ermöglicht die Neuvergabe der Freigabe. Available wird einfach auf false gesetzt und ein ggf. wartender Thread
+	 * wird wieder erweckt.
+	 */
 	public synchronized void V()
 	{
-		resource = true;
+		available = true;
 		notify();
 	}
 }
